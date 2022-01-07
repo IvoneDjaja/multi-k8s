4a757c02a4804430728a3535a87c41da42f204cf
IvoneDjaja <59554430+IvoneDjaja@users.noreply.github.com>
2022-01-07 18:43:39 +0800

    test change

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fib Calculator</h1>
-          <Link to="/">Home</Link>
+          <Link to="/">Test</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
